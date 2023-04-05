1b4ca6c275940ac9cc57aeca5317ca396e7e6f60
Chazz Gagliano <cgags17285@gmail.com>
2023-04-05 12:27:08 -0400

    seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "doritos", price: 4, image_url: "https://cdn11.bigcommerce.com/s-840m18ew7o/images/stencil/1280x1280/products/3279/4861/62737__97488.1593452043.jpg?c=1", description: "nacho cheese", quantity: 45},
+  {name: "cheetos", price: 2, image_url: "http://mobileimages.lowes.com/product/converted/028400/028400097604.jpg", description: "crunchy corn chips", quantity: 30},
+  {name: "potato_chips", price: 2, image_url: "https:/upload.wikimedia.org/wikipedia/commons/thumb/6/69/Potato-Chips.jpg/800px-Potato-Chips.jpg", description: "fried slice of potato", quantity: nil},
+  {name: "cheese puffs", price: 4, image_url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheese-Puffs_EXPS_TOHDJ23_34614_P2_MD_07_14_5b.jpg", description: "\"puffed corn snack with cheese\"", quantity: 55}
+])
