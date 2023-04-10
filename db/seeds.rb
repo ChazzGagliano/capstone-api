0512f9299de3e6cc3a15fcd084ed2024d2e077c6
Chazz Gagliano <cgags17285@gmail.com>
2023-04-10 14:12:27 -0400

    seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+Supplier.create!([
+  {name: "frito-lay", email: "frito-lay@email.com", phone_number: "7141220123"},
+  {name: "amazon", email: "amazon@email.com", phone_number: "81534\n11518"}
+])
 Product.create!([
-  {name: "doritos", price: 4, image_url: "https://cdn11.bigcommerce.com/s-840m18ew7o/images/stencil/1280x1280/products/3279/4861/62737__97488.1593452043.jpg?c=1", description: "nacho cheese", quantity: 45},
-  {name: "cheetos", price: 2, image_url: "http://mobileimages.lowes.com/product/converted/028400/028400097604.jpg", description: "crunchy corn chips", quantity: 30},
-  {name: "potato_chips", price: 2, image_url: "https:/upload.wikimedia.org/wikipedia/commons/thumb/6/69/Potato-Chips.jpg/800px-Potato-Chips.jpg", description: "fried slice of potato", quantity: nil},
-  {name: "cheese puffs", price: 4, image_url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheese-Puffs_EXPS_TOHDJ23_34614_P2_MD_07_14_5b.jpg", description: "\"puffed corn snack with cheese\"", quantity: 55}
+  {name: "doritos", price: 4, image_url: "https://cdn11.bigcommerce.com/s-840m18ew7o/images/stencil/1280x1280/products/3279/4861/62737__97488.1593452043.jpg?c=1", description: "nacho cheese", quantity: 45, supplier_id: 51},
+  {name: "cheetos", price: 2, image_url: "http://mobileimages.lowes.com/product/converted/028400/028400097604.jpg", description: "crunchy corn chips", quantity: 30, supplier_id: 52},
+  {name: "potato_chips", price: 2, image_url: "https:/upload.wikimedia.org/wikipedia/commons/thumb/6/69/Potato-Chips.jpg/800px-Potato-Chips.jpg", description: "fried slice of potato", quantity: 45, supplier_id: 53},
+  {name: "cheese puffs", price: 4, image_url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheese-Puffs_EXPS_TOHDJ23_34614_P2_MD_07_14_5b.jpg", description: "\"puffed corn snack with cheese\"", quantity: 55, supplier_id: 54}
 ])
