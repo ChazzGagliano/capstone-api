0ad2433965aa81a9112109c659e4e9fe4da55158
Chazz Gagliano <cgags17285@gmail.com>
2023-04-11 13:14:29 -0400

    remove image_url column

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,7 +1,6 @@
 json.id product.id
 json.name product.name
 json.price product.price
-json.image_url product.image_url
 json.description product.description
 json.flavor product.flavor
 json.tax product.tax
@@ -9,5 +8,6 @@ json.is_discounted product.is_discounted
 json.total product.total
 json.quantity product.quantity
 json.supplier_id product.supplier_id
+json.supplier product.supplier
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Supplier.create!([
   {name: "amazon", email: "amazon@email.com", phone_number: "81534\n11518"}
 ])
 Product.create!([
-  {name: "doritos", price: 4, image_url: "https://cdn11.bigcommerce.com/s-840m18ew7o/images/stencil/1280x1280/products/3279/4861/62737__97488.1593452043.jpg?c=1", description: "nacho cheese", quantity: 45, supplier_id: 51},
-  {name: "cheetos", price: 2, image_url: "http://mobileimages.lowes.com/product/converted/028400/028400097604.jpg", description: "crunchy corn chips", quantity: 30, supplier_id: 52},
-  {name: "potato_chips", price: 2, image_url: "https:/upload.wikimedia.org/wikipedia/commons/thumb/6/69/Potato-Chips.jpg/800px-Potato-Chips.jpg", description: "fried slice of potato", quantity: 45, supplier_id: 53},
-  {name: "cheese puffs", price: 4, image_url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheese-Puffs_EXPS_TOHDJ23_34614_P2_MD_07_14_5b.jpg", description: "\"puffed corn snack with cheese\"", quantity: 55, supplier_id: 54}
+  {name: "doritos", price: 4, image_url: "https://cdn11.bigcommerce.com/s-840m18ew7o/images/stencil/1280x1280/products/3279/4861/62737__97488.1593452043.jpg?c=1", description: "nacho cheese", quantity: 45, supplier_id: 1},
+  {name: "cheetos", price: 2, image_url: "http://mobileimages.lowes.com/product/converted/028400/028400097604.jpg", description: "crunchy corn chips", quantity: 30, supplier_id: 2},
+  {name: "potato_chips", price: 2, image_url: "https:/upload.wikimedia.org/wikipedia/commons/thumb/6/69/Potato-Chips.jpg/800px-Potato-Chips.jpg", description: "fried slice of potato", quantity: 45, supplier_id: 2},
+  {name: "cheese puffs", price: 4, image_url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Cheese-Puffs_EXPS_TOHDJ23_34614_P2_MD_07_14_5b.jpg", description: "\"puffed corn snack with cheese\"", quantity: 55, supplier_id: 1}
 ])
