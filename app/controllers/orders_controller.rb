df965fb764b2260cec45b34ce8641ff729cfc666
Chazz Gagliano <cgags17285@gmail.com>
2023-04-18 13:27:38 -0400

    create and read action for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  def show
+    @order = Order.find_by(id: params[:id])
+    render :show
+  end
+
+  def index
+    @orders = Order.all
+    render :index
+  end
+
+  def create
+    @order = Order.new(
+    user_id: current_user.id, 
+    product_id: params[:product_id],
+    quantity: params[:quantity],
+    subtotal: params[:subtotal]
+    )
+    if @order.save
+      render :show
+    else
+      render json: {errors: @order.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ post "/users" => "users#create"
 
 post "/sessions" => "sessions#create"
 
+get "/orders/:id" => "orders#show"
+get "/orders" => "orders#index"
+post "/orders" => "orders#create"
 end
